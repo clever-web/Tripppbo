b0ce28ca496700be7f2cb910abd774adfd5e4e5e
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-30 00:18:28 +0300

    Create GiftCardController.php

---
+++ app/Http/Controllers/GiftCardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GiftCard;
+use Illuminate\Http\Request;
+
+class GiftCardController extends Controller
+{
+    public function verifyGiftCard(Request $r)
+    {
+        $gift_card =   GiftCard::where('code', $r['code'])->where('active', true)->first();
+
+        if ($gift_card) {
+            return $gift_card;
+        } else {
+            abort(404, "gift card is not valid.");
+        }
+    }
+}
