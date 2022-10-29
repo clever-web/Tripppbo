f46f673dcfd0a2858c4daf02c979cdf6be827fcb
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-30 00:18:37 +0300

    Create LoginController.php

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FacebookUser;
+use App\Models\GoogleUser;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use function PHPUnit\Framework\isEmpty;
+
+class LoginController extends Controller
+{
+    //
+
+    public function registerWithGoogle(Request $request)
+    {
+        $googleUser = Socialite::driver('google')->user();
+
+        $id = $googleUser->getId();
+
+        $existingGoogleUser = GoogleUser::where('google_user_id', $id)->first();
+
+        if ($existingGoogleUser) {
+            Auth::login($existingGoogleUser->user);
+            $request->session()->regenerate();
+
+            return redirect()->intended(RouteServiceProvider::HOME);
+        } else {
+            $u = new User();
+            $u->name = $googleUser->getName();
+            $u->email = $googleUser->getEmail();
+            $u->save();
+
+            $gu = new GoogleUser();
+            $gu->user_id = $u->id;
+            $gu->google_user_id = $googleUser->getId();
+            $gu->save();
+
+            event(new Registered($u));
+
+            Auth::login($u);
+            $request->session()->regenerate();
+
+            return redirect()->intended(RouteServiceProvider::HOME);
+        }
+    }
+
+    public function registerWithFacebook(Request $request)
+    {
+        $facebookUser = Socialite::driver('facebook')->user();
+
+        $id = $facebookUser->getId();
+
+        $existingFacebookUser = FacebookUser::where('facebook_user_id', $id)->first();
+
+        if ($existingFacebookUser) {
+            Auth::login($existingFacebookUser->user);
+            $request->session()->regenerate();
+
+            return redirect()->intended(RouteServiceProvider::HOME);
+        } else {
+            $u = new User();
+            $u->name = $facebookUser->getName();
+            $u->email = $facebookUser->getEmail();
+            $u->save();
+
+            $fu = new FacebookUser();
+            $fu->user_id = $u->id;
+            $fu->facebook_user_id = $facebookUser->getId();
+            $fu->save();
+            event(new Registered($u));
+
+            Auth::login($u);
+            $request->session()->regenerate();
+
+            return redirect()->intended(RouteServiceProvider::HOME);
+        }
+    }
+}
