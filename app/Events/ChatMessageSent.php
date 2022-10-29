7ebc2ddbaf5c48c950af63210b878a0a17ca0673
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-29 07:50:56 +0300

    Create ChatMessageSent.php

---
+++ app/Events/ChatMessageSent.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\ChatMessage;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ChatMessageSent implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public $message;
+
+    public function __construct(ChatMessage $message)
+    {
+        //
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('user.'.$this->message->receiver_id);
+    }
+}
