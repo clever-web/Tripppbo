e0699ad50a3b866a07da36c67cbc1e75ce4a62be
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-30 00:18:42 +0300

    Create NotificationsController.php

---
+++ app/Http/Controllers/NotificationsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NotificationsController extends Controller
+{
+    public function markAllAsRead(Request $r)
+    {
+        $user = $r->user();
+        $user->unreadNotifications()->update(['read_at' => now()]);
+    }
+}
