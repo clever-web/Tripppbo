6abc6002b6fdab2af166ec939dbe10fb350f8381
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-31 00:18:48 +0200

    Update EmailVerificationNotificationController.php

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -1,27 +1,27 @@
-<?php
+    <?php
 
-namespace App\Http\Controllers\Auth;
+    namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Http\Request;
+    use App\Http\Controllers\Controller;
+    use App\Providers\RouteServiceProvider;
+    use Illuminate\Http\Request;
 
-class EmailVerificationNotificationController extends Controller
-{
-    /**
-     * Send a new email verification notification.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    class EmailVerificationNotificationController extends Controller
     {
-        if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(RouteServiceProvider::HOME);
+        /**
+         * Send a new email verification notification.
+         *
+         * @param  \Illuminate\Http\Request  $request
+         * @return \Illuminate\Http\Response
+         */
+        public function store(Request $request)
+        {
+            if ($request->user()->hasVerifiedEmail()) {
+                return redirect()->intended(RouteServiceProvider::HOME);
+            }
+
+            $request->user()->sendEmailVerificationNotification();
+
+            return back()->with('status', 'verification-link-sent');
         }
-
-        $request->user()->sendEmailVerificationNotification();
-
-        return back()->with('status', 'verification-link-sent');
     }
-}
