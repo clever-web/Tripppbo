c618b9b2cc938f93b1eb3277da188dc1bd433455
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-31 00:18:44 +0200

    Update AuthenticatedSessionController.php

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -1,54 +1,54 @@
-<?php
+    <?php
 
-namespace App\Http\Controllers\Auth;
+    namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+    use App\Http\Controllers\Controller;
+    use App\Http\Requests\Auth\LoginRequest;
+    use App\Providers\RouteServiceProvider;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Auth;
 
-class AuthenticatedSessionController extends Controller
-{
-    /**
-     * Display the login view.
-     *
-     * @return \Illuminate\View\View
-     */
-    public function create()
+    class AuthenticatedSessionController extends Controller
     {
-        return view('auth.login');
+        /**
+         * Display the login view.
+         *
+         * @return \Illuminate\View\View
+         */
+        public function create()
+        {
+            return view('auth.login');
+        }
+
+        /**
+         * Handle an incoming authentication request.
+         *
+         * @param  \App\Http\Requests\Auth\LoginRequest  $request
+         * @return \Illuminate\Http\RedirectResponse
+         */
+        public function store(LoginRequest $request)
+        {
+            $request->authenticate();
+
+            $request->session()->regenerate();
+
+            return redirect()->intended(RouteServiceProvider::HOME);
+        }
+
+        /**
+         * Destroy an authenticated session.
+         *
+         * @param  \Illuminate\Http\Request  $request
+         * @return \Illuminate\Http\RedirectResponse
+         */
+        public function destroy(Request $request)
+        {
+            Auth::guard('web')->logout();
+
+            $request->session()->invalidate();
+
+            $request->session()->regenerateToken();
+
+            return redirect('/');
+        }
     }
-
-    /**
-     * Handle an incoming authentication request.
-     *
-     * @param  \App\Http\Requests\Auth\LoginRequest  $request
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function store(LoginRequest $request)
-    {
-        $request->authenticate();
-
-        $request->session()->regenerate();
-
-        return redirect()->intended(RouteServiceProvider::HOME);
-    }
-
-    /**
-     * Destroy an authenticated session.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function destroy(Request $request)
-    {
-        Auth::guard('web')->logout();
-
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
-        return redirect('/');
-    }
-}
