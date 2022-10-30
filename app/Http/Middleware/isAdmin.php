31611d196ef05b79607086c8407f7791c025de6e
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-30 03:16:10 +0200

    Create isAdmin.php

---
+++ app/Http/Middleware/isAdmin.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class isAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check()) {
+            if ($request->user()->isAdmin) {
+                return $next($request);
+            } else {
+                abort(404);
+            }
+        } else {
+            abort(404);
+        }
+    }
+}
