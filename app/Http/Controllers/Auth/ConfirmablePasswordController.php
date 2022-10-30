0bd5dfef97f936117daa3175442e1e64ef6ee913
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-31 00:18:46 +0200

    Update ConfirmablePasswordController.php

--- app/Http/Controllers/Auth/ConfirmablePasswordController.php
+++ app/Http/Controllers/Auth/ConfirmablePasswordController.php
@@ -1,45 +1,45 @@
-<?php
+    <?php
 
-namespace App\Http\Controllers\Auth;
+    namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Validation\ValidationException;
+    use App\Http\Controllers\Controller;
+    use App\Providers\RouteServiceProvider;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Auth;
+    use Illuminate\Validation\ValidationException;
 
-class ConfirmablePasswordController extends Controller
-{
-    /**
-     * Show the confirm password view.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\View\View
-     */
-    public function show(Request $request)
+    class ConfirmablePasswordController extends Controller
     {
-        return view('auth.confirm-password');
-    }
-
-    /**
-     * Confirm the user's password.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return mixed
-     */
-    public function store(Request $request)
-    {
-        if (! Auth::guard('web')->validate([
-            'email' => $request->user()->email,
-            'password' => $request->password,
-        ])) {
-            throw ValidationException::withMessages([
-                'password' => __('auth.password'),
-            ]);
+        /**
+         * Show the confirm password view.
+         *
+         * @param  \Illuminate\Http\Request  $request
+         * @return \Illuminate\View\View
+         */
+        public function show(Request $request)
+        {
+            return view('auth.confirm-password');
         }
 
-        $request->session()->put('auth.password_confirmed_at', time());
+        /**
+         * Confirm the user's password.
+         *
+         * @param  \Illuminate\Http\Request  $request
+         * @return mixed
+         */
+        public function store(Request $request)
+        {
+            if (! Auth::guard('web')->validate([
+                'email' => $request->user()->email,
+                'password' => $request->password,
+            ])) {
+                throw ValidationException::withMessages([
+                    'password' => __('auth.password'),
+                ]);
+            }
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+            $request->session()->put('auth.password_confirmed_at', time());
+
+            return redirect()->intended(RouteServiceProvider::HOME);
+        }
     }
-}
