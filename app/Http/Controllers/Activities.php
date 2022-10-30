505c00269cbd9fd2791b2c3d6a55ba28217c12ee
superstar <83311833+clever-web@users.noreply.github.com>
2022-10-31 00:18:33 +0200

    Update Activities.php

--- app/Http/Controllers/Activities.php
+++ app/Http/Controllers/Activities.php
@@ -1,10 +1,10 @@
-<?php
+    <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+    use Illuminate\Http\Request;
 
-class Activities extends Controller
-{
-    //
-}
+    class Activities extends Controller
+    {
+        //
+    }
