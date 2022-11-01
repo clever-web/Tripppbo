92e09d41a1024a599744b122ea3af119d326a668
superstar <83311833+clever-web@users.noreply.github.com>
2022-11-01 10:23:37 +0200

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -1,56 +1,56 @@
-<?php
+    <?php
 
-namespace App\Http\Controllers\Auth;
+    namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Models\User;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
+    use App\Http\Controllers\Controller;
+    use App\Models\User;
+    use App\Providers\RouteServiceProvider;
+    use Illuminate\Auth\Events\Registered;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Auth;
+    use Illuminate\Support\Facades\Hash;
 
-class RegisteredUserController extends Controller
-{
-    /**
-     * Display the registration view.
-     *
-     * @return \Illuminate\View\View
-     */
-    public function create()
+    class RegisteredUserController extends Controller
     {
-        return view('auth.register');
-    }
-
-    /**
-     * Handle an incoming registration request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function store(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8',
-            'terms_and_conditions' => 'required',
-        ]);
-
-        if ($request->input('terms_and_conditions') !== 'agreed') {
-            abort(403);
+        /**
+         * Display the registration view.
+         *
+         * @return \Illuminate\View\View
+         */
+        public function create()
+        {
+            return view('auth.register');
         }
 
-        Auth::login($user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]));
+        /**
+         * Handle an incoming registration request.
+         *
+         * @param  \Illuminate\Http\Request  $request
+         * @return \Illuminate\Http\RedirectResponse
+         *
+         * @throws \Illuminate\Validation\ValidationException
+         */
+        public function store(Request $request)
+        {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:8',
+                'terms_and_conditions' => 'required',
+            ]);
 
-        event(new Registered($user));
+            if ($request->input('terms_and_conditions') !== 'agreed') {
+                abort(403);
+            }
 
-        return redirect(RouteServiceProvider::HOME);
+            Auth::login($user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]));
+
+            event(new Registered($user));
+
+            return redirect(RouteServiceProvider::HOME);
+        }
     }
-}
