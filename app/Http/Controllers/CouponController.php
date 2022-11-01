fda893866aaa1dcfa57dd6b423e72cb7cb850529
superstar <83311833+clever-web@users.noreply.github.com>
2022-11-01 10:23:52 +0200

    Update CouponController.php

--- app/Http/Controllers/CouponController.php
+++ app/Http/Controllers/CouponController.php
@@ -1,26 +1,26 @@
-<?php
+    <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-use App\Models\CouponCode;
-use Illuminate\Http\Request;
+    use App\Models\CouponCode;
+    use Illuminate\Http\Request;
 
-class CouponController extends Controller
-{
-    public function verifyCoupon(Request $r)
+    class CouponController extends Controller
     {
-        $coupon =    CouponCode::where('coupon_code', $r['coupon_code'])->first();
+        public function verifyCoupon(Request $r)
+        {
+            $coupon =    CouponCode::where('coupon_code', $r['coupon_code'])->first();
 
-        if ($coupon)
-        {
-            return $coupon;
+            if ($coupon)
+            {
+                return $coupon;
+            }
+            else
+            {
+                abort("404", "Coupon code was not found");
+            }
         }
-        else
-        {
-            abort("404", "Coupon code was not found");
-        }
+
+
+
     }
-
-
-
-}
