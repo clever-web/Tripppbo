b2de31d82ddce602ef83b174722f677ce4cf9676
superstar <83311833+clever-web@users.noreply.github.com>
2022-11-02 06:51:08 +0200

    Update PaymentController.php

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -1,41 +1,41 @@
-<?php
+    <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-use App\Models\User;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+    use App\Models\User;
+    use Illuminate\Database\Eloquent\Collection;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Auth;
 
-class PaymentController extends Controller
-{
-    public function index()
+    class PaymentController extends Controller
     {
-        $user = User::findOrFail(Auth::id());
-        $user->createOrGetStripeCustomer();
-        $paymentMethods = $user->paymentMethods();
-        if ($paymentMethods->isEmpty()) {
-            $paymentMethods = new Collection();
+        public function index()
+        {
+            $user = User::findOrFail(Auth::id());
+            $user->createOrGetStripeCustomer();
+            $paymentMethods = $user->paymentMethods();
+            if ($paymentMethods->isEmpty()) {
+                $paymentMethods = new Collection();
+            }
+
+            return view('payments.payment-methods', ['user' => $user, 'payment_methods' => $paymentMethods, 'intent' => $user->createSetupIntent()]);
         }
 
-        return view('payments.payment-methods', ['user' => $user, 'payment_methods' => $paymentMethods, 'intent' => $user->createSetupIntent()]);
+        public function store(Request $request)
+        {
+            $user = $request->user();
+            $paymentMethod = $request['payment_method_id'];
+            $user->addPaymentMethod($paymentMethod);
+
+            return redirect()->route('payment-methods-home');
+        }
+
+        public function delete(Request $request)
+        {
+            $user = $request->user();
+            $paymentMethod = $user->findPaymentMethod($request['payment_method_id']);
+            $paymentMethod->delete();
+
+            return redirect()->route('payment-methods-home');
+        }
     }
-
-    public function store(Request $request)
-    {
-        $user = $request->user();
-        $paymentMethod = $request['payment_method_id'];
-        $user->addPaymentMethod($paymentMethod);
-
-        return redirect()->route('payment-methods-home');
-    }
-
-    public function delete(Request $request)
-    {
-        $user = $request->user();
-        $paymentMethod = $user->findPaymentMethod($request['payment_method_id']);
-        $paymentMethod->delete();
-
-        return redirect()->route('payment-methods-home');
-    }
-}
