b4d0d93aace423a0c53f643ac5f9ee2e7a8a38b0
superstar <83311833+clever-web@users.noreply.github.com>
2022-11-10 08:55:27 +0200

    Update booking-summary.blade.php

--- resources/views/components/flight/booking-summary.blade.php
+++ resources/views/components/flight/booking-summary.blade.php
@@ -1,29 +1,29 @@
-<section class="container-all-booking mb-3">
-    <header class="bg-fafafa p-4 hsm">
-        <h4 class="gilroy-bold font-28 mb-2">Passenger</h4>
-        <p class="gilroy-regular font-12 mb-0">Why is flying?</p>
-    </header>
-    <div>
-        <div class="rounded-lg p-4">
-            <div class="grid-5">
-                <div>
-                    <h4 class="gilroy-bold text-fe2f70 font-20 mb-2">E-Ticket</h4>
-                    <p class="gilroy-regular font-14 mb-3">Sent at 130-07-2021 12:13 to test@gmail.com</p>
-                    <ul class="list-7 d-flex flex-column">
-                        <li><button class="btn"><span>Resend</span><i class="fas fa-envelope" aria-hidden="true"></i></button></li>
-                        <li><button class="btn"><span>View & print</span><i class="fas fa-envelope" aria-hidden="true"></i></button></li>
-                    </ul>
-                </div>
-                <div>
-                    <h4 class="gilroy-bold text-fe2f70 font-20 mb-2">Booking Request Acknowledgement</h4>
-                    <p class="gilroy-regular font-14 mb-3">Sent at 130-07-2021 12:13 to test@gmail.com</p>
-                    <ul class="list-7 d-flex flex-column">
-                        <li><button class="btn"><span>Resend</span><i class="fas fa-envelope" aria-hidden="true"></i></button></li>
-                        <li><button class="btn"><span>View & print</span><i class="fas fa-envelope" aria-hidden="true"></i></button></li>
-                    </ul>
+    <section class="container-all-booking mb-3">
+        <header class="bg-fafafa p-4 hsm">
+            <h4 class="gilroy-bold font-28 mb-2">Passenger</h4>
+            <p class="gilroy-regular font-12 mb-0">Why is flying?</p>
+        </header>
+        <div>
+            <div class="rounded-lg p-4">
+                <div class="grid-5">
+                    <div>
+                        <h4 class="gilroy-bold text-fe2f70 font-20 mb-2">E-Ticket</h4>
+                        <p class="gilroy-regular font-14 mb-3">Sent at 130-07-2021 12:13 to test@gmail.com</p>
+                        <ul class="list-7 d-flex flex-column">
+                            <li><button class="btn"><span>Resend</span><i class="fas fa-envelope" aria-hidden="true"></i></button></li>
+                            <li><button class="btn"><span>View & print</span><i class="fas fa-envelope" aria-hidden="true"></i></button></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h4 class="gilroy-bold text-fe2f70 font-20 mb-2">Booking Request Acknowledgement</h4>
+                        <p class="gilroy-regular font-14 mb-3">Sent at 130-07-2021 12:13 to test@gmail.com</p>
+                        <ul class="list-7 d-flex flex-column">
+                            <li><button class="btn"><span>Resend</span><i class="fas fa-envelope" aria-hidden="true"></i></button></li>
+                            <li><button class="btn"><span>View & print</span><i class="fas fa-envelope" aria-hidden="true"></i></button></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="text-center pt-0"><a href="javascript:void(0)" class="btn gilroy-bold text-fe2f70 text-decoration-underline font-12 p-0">View payment overview(s)</a></div>
-</section>
+        <div class="text-center pt-0"><a href="javascript:void(0)" class="btn gilroy-bold text-fe2f70 text-decoration-underline font-12 p-0">View payment overview(s)</a></div>
+    </section>
