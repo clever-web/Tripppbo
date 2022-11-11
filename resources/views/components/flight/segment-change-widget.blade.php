6a7f0f38f71f618ccf61df6b1397f94c0cff1a9f
superstar <83311833+clever-web@users.noreply.github.com>
2022-11-11 07:44:23 +0200

    Update segment-change-widget.blade.php

--- resources/views/components/flight/segment-change-widget.blade.php
+++ resources/views/components/flight/segment-change-widget.blade.php
@@ -1,85 +1,85 @@
-<section class="container-all-booking mb-3">
-    <button type="button" onclick="openTab(event, 'all-flights');" class="btn btn-block gilroy-semi text-white text-left font-14 btn-fe2f70 py-3 hlg">
-        <i class="fas fa-arrow-left mr-1" aria-hidden="true"></i> Back
-    </button>
-    <header class="bg-fafafa p-4">
-        <button type="button" onclick="openTab(event, 'all-flights');" class="btn btn-block gilroy-bold text-left text-fe2f70 font-20 mb-3 hsm">
-            <i class="fas fa-arrow-left mr-1" aria-hidden="true"></i> Change Flight
+    <section class="container-all-booking mb-3">
+        <button type="button" onclick="openTab(event, 'all-flights');" class="btn btn-block gilroy-semi text-white text-left font-14 btn-fe2f70 py-3 hlg">
+            <i class="fas fa-arrow-left mr-1" aria-hidden="true"></i> Back
         </button>
-        <h4 class="gilroy-bold font-28 mb-2">Change Flight</h4>
-        <p class="gilroy-regular font-12 mb-0">Please select the flight that you want to change</p>
-    </header>
-    <div class="pt-0">
-        <div class="rounded-lg p-4 mb-3">
-            <div class="d-flex mb-3">
-                <div><label class="trippbo-checkbox gilroy-regular mt-2"><input type="checkbox" name=""><span></span></label></div>
-                <div>
-                    <div class="d-flex flex-column">
-                        <div><span class="gilroy-regular font-12 mr-3">Flight No:</span> <span class="gilroy-bold font-12">PC1072</span></div>
-                        <div><span class="gilroy-regular font-12 mr-3">Flight Date</span> <span class="gilroy-bold font-12">24 Aug 2021</span></div>
-                        <div></div>
-                    </div>
-                </div>
-            </div>
-            <div class="timeline-flight-change mb-3">
-                <div>
-                    <div class="timeline-container">
-                        <div></div>
-                        <div></div>
-                        <div>
-                            <h4 class="gilroy-medium font-12 opacity-0-5 mb-1">Copenhagen</h4>
-                            <p class="gilroy-bold font-16 mb-0">12:55</p>
+        <header class="bg-fafafa p-4">
+            <button type="button" onclick="openTab(event, 'all-flights');" class="btn btn-block gilroy-bold text-left text-fe2f70 font-20 mb-3 hsm">
+                <i class="fas fa-arrow-left mr-1" aria-hidden="true"></i> Change Flight
+            </button>
+            <h4 class="gilroy-bold font-28 mb-2">Change Flight</h4>
+            <p class="gilroy-regular font-12 mb-0">Please select the flight that you want to change</p>
+        </header>
+        <div class="pt-0">
+            <div class="rounded-lg p-4 mb-3">
+                <div class="d-flex mb-3">
+                    <div><label class="trippbo-checkbox gilroy-regular mt-2"><input type="checkbox" name=""><span></span></label></div>
+                    <div>
+                        <div class="d-flex flex-column">
+                            <div><span class="gilroy-regular font-12 mr-3">Flight No:</span> <span class="gilroy-bold font-12">PC1072</span></div>
+                            <div><span class="gilroy-regular font-12 mr-3">Flight Date</span> <span class="gilroy-bold font-12">24 Aug 2021</span></div>
+                            <div></div>
                         </div>
                     </div>
                 </div>
-                <div>
-                    <div class="timeline-container">
-                        <div></div>
-                        <div></div>
-                        <div>
-                            <h4 class="gilroy-medium font-12 opacity-0-5 mb-1">Istanbul.Gokcen</h4>
-                            <p class="gilroy-bold font-16 mb-0">17:55</p>
+                <div class="timeline-flight-change mb-3">
+                    <div>
+                        <div class="timeline-container">
+                            <div></div>
+                            <div></div>
+                            <div>
+                                <h4 class="gilroy-medium font-12 opacity-0-5 mb-1">Copenhagen</h4>
+                                <p class="gilroy-bold font-16 mb-0">12:55</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="timeline-container">
+                            <div></div>
+                            <div></div>
+                            <div>
+                                <h4 class="gilroy-medium font-12 opacity-0-5 mb-1">Istanbul.Gokcen</h4>
+                                <p class="gilroy-bold font-16 mb-0">17:55</p>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div class="gilroy-bold font-14 bg-fafafa rounded-lg p-3">CPH-SAW-GZT</div>	
             </div>
-            <div class="gilroy-bold font-14 bg-fafafa rounded-lg p-3">CPH-SAW-GZT</div>	
+            <div class="rounded-lg p-4">
+                <div class="d-flex mb-3">
+                    <div><label class="trippbo-checkbox gilroy-regular mt-2"><input type="checkbox" name=""><span></span></label></div>
+                    <div>
+                        <div class="d-flex flex-column">
+                            <div><span class="gilroy-regular font-12 mr-3">Flight No:</span> <span class="gilroy-bold font-12">PC1072</span></div>
+                            <div><span class="gilroy-regular font-12 mr-3">Flight Date</span> <span class="gilroy-bold font-12">24 Aug 2021</span></div>
+                            <div></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="timeline-flight-change mb-3">
+                    <div>
+                        <div class="timeline-container">
+                            <div></div>
+                            <div></div>
+                            <div>
+                                <h4 class="gilroy-medium font-12 opacity-0-5 mb-1">Copenhagen</h4>
+                                <p class="gilroy-bold font-16 mb-0">12:55</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div class="timeline-container">
+                            <div></div>
+                            <div></div>
+                            <div>
+                                <h4 class="gilroy-medium font-12 opacity-0-5 mb-1">Istanbul.Gokcen</h4>
+                                <p class="gilroy-bold font-16 mb-0">17:55</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="gilroy-bold font-14 bg-fafafa rounded-lg p-3">CPH-SAW-GZT</div>	
+            </div>
         </div>
-        <div class="rounded-lg p-4">
-            <div class="d-flex mb-3">
-                <div><label class="trippbo-checkbox gilroy-regular mt-2"><input type="checkbox" name=""><span></span></label></div>
-                <div>
-                    <div class="d-flex flex-column">
-                        <div><span class="gilroy-regular font-12 mr-3">Flight No:</span> <span class="gilroy-bold font-12">PC1072</span></div>
-                        <div><span class="gilroy-regular font-12 mr-3">Flight Date</span> <span class="gilroy-bold font-12">24 Aug 2021</span></div>
-                        <div></div>
-                    </div>
-                </div>
-            </div>
-            <div class="timeline-flight-change mb-3">
-                <div>
-                    <div class="timeline-container">
-                        <div></div>
-                        <div></div>
-                        <div>
-                            <h4 class="gilroy-medium font-12 opacity-0-5 mb-1">Copenhagen</h4>
-                            <p class="gilroy-bold font-16 mb-0">12:55</p>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div class="timeline-container">
-                        <div></div>
-                        <div></div>
-                        <div>
-                            <h4 class="gilroy-medium font-12 opacity-0-5 mb-1">Istanbul.Gokcen</h4>
-                            <p class="gilroy-bold font-16 mb-0">17:55</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="gilroy-bold font-14 bg-fafafa rounded-lg p-3">CPH-SAW-GZT</div>	
-        </div>
-    </div>
-    <div class="d-flex justify-content-end pt-0"><button type="button" class="btn gilroy-bold btn-fe2f70 font-18 px-5">Conitnue</button></div>
-</section>
+        <div class="d-flex justify-content-end pt-0"><button type="button" class="btn gilroy-bold btn-fe2f70 font-18 px-5">Conitnue</button></div>
+    </section>
