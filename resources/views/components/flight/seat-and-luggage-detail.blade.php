5083440bde29cb229a03db93800be76808718958
superstar <83311833+clever-web@users.noreply.github.com>
2022-11-11 07:44:16 +0200

    Update seat-and-luggage-detail.blade.php

--- resources/views/components/flight/seat-and-luggage-detail.blade.php
+++ resources/views/components/flight/seat-and-luggage-detail.blade.php
@@ -1,76 +1,76 @@
-<section class="container-all-booking">
-    <div>
-        <div class="rounded-lg p-4 mb-3">
-            <ul class="grid-8">
-                <li>
-                    <div class="d-flex flex-row-responsive">
-                        <div><img class="icon-40px" src="/image/64455.png"></div>
-                        <div>
-                            <h4 class="gilroy-semi text-000941 font-18 mb-2">Special Luggage</h4>
-                            <p class="gilroy-medium font-12 mb-2">Medical Luggage <i class="fas fa-check text-56d18f icon-14px ml-2" aria-hidden="true"></i></p>
-                            <p class="gilroy-medium font-12 mb-0">Wheel Chair <i class="fas fa-check text-56d18f icon-14px ml-2" aria-hidden="true"></i></p>
+    <section class="container-all-booking">
+        <div>
+            <div class="rounded-lg p-4 mb-3">
+                <ul class="grid-8">
+                    <li>
+                        <div class="d-flex flex-row-responsive">
+                            <div><img class="icon-40px" src="/image/64455.png"></div>
+                            <div>
+                                <h4 class="gilroy-semi text-000941 font-18 mb-2">Special Luggage</h4>
+                                <p class="gilroy-medium font-12 mb-2">Medical Luggage <i class="fas fa-check text-56d18f icon-14px ml-2" aria-hidden="true"></i></p>
+                                <p class="gilroy-medium font-12 mb-0">Wheel Chair <i class="fas fa-check text-56d18f icon-14px ml-2" aria-hidden="true"></i></p>
+                            </div>
                         </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="d-flex">
-                        <div><p class="gilroy-medium font-12 mb-0">Need Something<br />Special</p></div>
-                        <div><button class="pressed">Contact Us</button></div>
-                    </div>
-                </li>
-            </ul>
-        </div>
-        <div class="rounded-lg p-4">
-            <div class="d-flex align-items-center mb-3">
-                <div class="mr-3"><img class="icon-40px" src="/image/64430.png"></div>
-                <div><h4 class="gilroy-semi text-000941 font-18">Seats</h4></div>
+                    </li>
+                    <li>
+                        <div class="d-flex">
+                            <div><p class="gilroy-medium font-12 mb-0">Need Something<br />Special</p></div>
+                            <div><button class="pressed">Contact Us</button></div>
+                        </div>
+                    </li>
+                </ul>
             </div>
-            <div class="gilroy-bold font-14 bg-fafafa rounded-lg mb-3 p-3">
-                <div class="d-flex align-items-center">
-                    <div class="mr-3"><img class="icon-24px" src="/image/64448.png"></div>
-                    <div class="mr-2"><h4 class="gilroy-bold font-14 mb-0">Dubai To Saudi Arabia</h4></div>
-                    <div><h4 class="gilroy-regular font-14 mb-0">- Mon 23 Aug</h4></div>
+            <div class="rounded-lg p-4">
+                <div class="d-flex align-items-center mb-3">
+                    <div class="mr-3"><img class="icon-40px" src="/image/64430.png"></div>
+                    <div><h4 class="gilroy-semi text-000941 font-18">Seats</h4></div>
                 </div>
-            </div>
-            <ul class="list-2">
-                <li>
-                    <div class="d-flex flex-row-responsive">
-                        <div class="mr-5">
-                            <h4 class="gilroy-bold font-16 mb-3">Passenger</h4>
-                            <p class="gilroy-medium font-12 mb-2">Nour Rajoub <i class="fas fa-check text-56d18f icon-14px ml-2" aria-hidden="true"></i></p>
-                            <p class="gilroy-medium font-12 mb-0">John Doe <i class="fas fa-check text-56d18f icon-14px ml-2" aria-hidden="true"></i></p>
-                        </div>
-                        <div>
-                            <h4 class="gilroy-bold font-16 mb-3">Hand Luggage</h4>
-                            <p class="gilroy-medium font-12 mb-2">Hand luggage: Included <i class="fas fa-times text-fe2f70 icon-14px ml-2" aria-hidden="true"></i></p>
-                            <p class="gilroy-medium font-12 mb-0">Hand luggage: Included <i class="fas fa-times text-fe2f70 icon-14px ml-2" aria-hidden="true"></i></p>
-                        </div>
+                <div class="gilroy-bold font-14 bg-fafafa rounded-lg mb-3 p-3">
+                    <div class="d-flex align-items-center">
+                        <div class="mr-3"><img class="icon-24px" src="/image/64448.png"></div>
+                        <div class="mr-2"><h4 class="gilroy-bold font-14 mb-0">Dubai To Saudi Arabia</h4></div>
+                        <div><h4 class="gilroy-regular font-14 mb-0">- Mon 23 Aug</h4></div>
                     </div>
-                </li>
-            </ul>
-            <div class="gilroy-bold font-14 bg-fafafa rounded-lg mb-3 p-3">
-                <div class="d-flex align-items-center">
-                    <div class="mr-3"><img class="icon-24px" src="/image/64448.png"></div>
-                    <div class="mr-2"><h4 class="gilroy-bold font-14 mb-0">Saudi Arabia To Dubai</h4></div>
-                    <div><h4 class="gilroy-regular font-14 mb-0">- Mon 23 Aug</h4></div>
                 </div>
-            </div>
-            <ul class="list-2">
-                <li>
-                    <div class="d-flex flex-row-responsive">
-                        <div class="mr-5">
-                            <h4 class="gilroy-bold font-16 mb-3">Passenger</h4>
-                            <p class="gilroy-medium font-12 mb-2">Nour Rajoub <i class="fas fa-check text-56d18f icon-14px ml-2" aria-hidden="true"></i></p>
-                            <p class="gilroy-medium font-12 mb-0">John Doe <i class="fas fa-check text-56d18f icon-14px ml-2" aria-hidden="true"></i></p>
+                <ul class="list-2">
+                    <li>
+                        <div class="d-flex flex-row-responsive">
+                            <div class="mr-5">
+                                <h4 class="gilroy-bold font-16 mb-3">Passenger</h4>
+                                <p class="gilroy-medium font-12 mb-2">Nour Rajoub <i class="fas fa-check text-56d18f icon-14px ml-2" aria-hidden="true"></i></p>
+                                <p class="gilroy-medium font-12 mb-0">John Doe <i class="fas fa-check text-56d18f icon-14px ml-2" aria-hidden="true"></i></p>
+                            </div>
+                            <div>
+                                <h4 class="gilroy-bold font-16 mb-3">Hand Luggage</h4>
+                                <p class="gilroy-medium font-12 mb-2">Hand luggage: Included <i class="fas fa-times text-fe2f70 icon-14px ml-2" aria-hidden="true"></i></p>
+                                <p class="gilroy-medium font-12 mb-0">Hand luggage: Included <i class="fas fa-times text-fe2f70 icon-14px ml-2" aria-hidden="true"></i></p>
+                            </div>
                         </div>
-                        <div>
-                            <h4 class="gilroy-bold font-16 mb-3">Hand Luggage</h4>
-                            <p class="gilroy-medium font-12 mb-2">Hand luggage: Included <i class="fas fa-times text-fe2f70 icon-14px ml-2" aria-hidden="true"></i></p>
-                            <p class="gilroy-medium font-12 mb-0">Hand luggage: Included <i class="fas fa-times text-fe2f70 icon-14px ml-2" aria-hidden="true"></i></p>
-                        </div>
+                    </li>
+                </ul>
+                <div class="gilroy-bold font-14 bg-fafafa rounded-lg mb-3 p-3">
+                    <div class="d-flex align-items-center">
+                        <div class="mr-3"><img class="icon-24px" src="/image/64448.png"></div>
+                        <div class="mr-2"><h4 class="gilroy-bold font-14 mb-0">Saudi Arabia To Dubai</h4></div>
+                        <div><h4 class="gilroy-regular font-14 mb-0">- Mon 23 Aug</h4></div>
                     </div>
-                </li>
-            </ul>
+                </div>
+                <ul class="list-2">
+                    <li>
+                        <div class="d-flex flex-row-responsive">
+                            <div class="mr-5">
+                                <h4 class="gilroy-bold font-16 mb-3">Passenger</h4>
+                                <p class="gilroy-medium font-12 mb-2">Nour Rajoub <i class="fas fa-check text-56d18f icon-14px ml-2" aria-hidden="true"></i></p>
+                                <p class="gilroy-medium font-12 mb-0">John Doe <i class="fas fa-check text-56d18f icon-14px ml-2" aria-hidden="true"></i></p>
+                            </div>
+                            <div>
+                                <h4 class="gilroy-bold font-16 mb-3">Hand Luggage</h4>
+                                <p class="gilroy-medium font-12 mb-2">Hand luggage: Included <i class="fas fa-times text-fe2f70 icon-14px ml-2" aria-hidden="true"></i></p>
+                                <p class="gilroy-medium font-12 mb-0">Hand luggage: Included <i class="fas fa-times text-fe2f70 icon-14px ml-2" aria-hidden="true"></i></p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
