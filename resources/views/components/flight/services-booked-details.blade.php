384dd76b4d5eb3d135f6114981084a2f2464d0b2
superstar <83311833+clever-web@users.noreply.github.com>
2022-11-11 07:44:28 +0200

    Update services-booked-details.blade.php

--- resources/views/components/flight/services-booked-details.blade.php
+++ resources/views/components/flight/services-booked-details.blade.php
@@ -1,34 +1,34 @@
-<section class="container-all-booking mb-3">
-    <div>
-        <div class="rounded-lg p-4 mb-3">
-            <div class="grid-7">
-                <div><img src="/image/64427.png" /></div>
-                <div>
-                    <h4 class="gilroy-bold text-000941 font-18 mb-2">Ticket Services</h4>
+    <section class="container-all-booking mb-3">
+        <div>
+            <div class="rounded-lg p-4 mb-3">
+                <div class="grid-7">
+                    <div><img src="/image/64427.png" /></div>
                     <div>
+                        <h4 class="gilroy-bold text-000941 font-18 mb-2">Ticket Services</h4>
+                        <div>
+                            <div><i class="fas fa-check text-56d18f icon-14px mr-2" aria-hidden="true"></i></div>
+                            <div>
+                                <p class="gilroy-medium font-12 mb-0">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+                                <a href="javascript:void(0)" class="btn gilroy-regular text-decoration-underline font-14 p-0">More information</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="rounded-lg p-4">
+                <div class="grid-7">
+                    <div><img src="/image/64423.png" /></div>
+                    <div>
+                        <h4 class="gilroy-bold text-000941 font-18 mb-2">Service package Basic</h4>
+                        <div>
                         <div><i class="fas fa-check text-56d18f icon-14px mr-2" aria-hidden="true"></i></div>
-                        <div>
-                            <p class="gilroy-medium font-12 mb-0">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
-                            <a href="javascript:void(0)" class="btn gilroy-regular text-decoration-underline font-14 p-0">More information</a>
+                            <div>
+                                <p class="gilroy-medium font-12 mb-0">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+                                <a href="javascript:void(0)" class="btn gilroy-regular text-decoration-underline font-14 p-0">More information</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="rounded-lg p-4">
-            <div class="grid-7">
-                <div><img src="/image/64423.png" /></div>
-                <div>
-                    <h4 class="gilroy-bold text-000941 font-18 mb-2">Service package Basic</h4>
-                    <div>
-                    <div><i class="fas fa-check text-56d18f icon-14px mr-2" aria-hidden="true"></i></div>
-                        <div>
-                            <p class="gilroy-medium font-12 mb-0">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
-                            <a href="javascript:void(0)" class="btn gilroy-regular text-decoration-underline font-14 p-0">More information</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+    </section>
