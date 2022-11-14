8d2ea7a9a01fb218df6f8f4967eec13c2b8bc2c0
superstar <83311833+clever-web@users.noreply.github.com>
2022-11-14 09:43:46 +0200

    Update chat-window.blade.php

--- resources/views/custom-components/chat-window.blade.php
+++ resources/views/custom-components/chat-window.blade.php
@@ -1,4 +1,4 @@
-<div id="chat_app" class="d-flex flex-row row-reverse" style="gap:20px;position: fixed; bottom:0px;right:100px;z-index:100;" >
-  <chat-window @messagesent="messageSentHandler" v-for="chat in chat_windows" :my_profile_pic_url="my_profile_pic" :user_id="chat.user_id" :my_user_id="chat.my_user_id" :ref="'user_' + chat.user_id" ></chat-window>
+  <div id="chat_app" class="d-flex flex-row row-reverse" style="gap:20px;position: fixed; bottom:0px;right:100px;z-index:100;" >
+    <chat-window @messagesent="messageSentHandler" v-for="chat in chat_windows" :my_profile_pic_url="my_profile_pic" :user_id="chat.user_id" :my_user_id="chat.my_user_id" :ref="'user_' + chat.user_id" ></chat-window>
 
-</div>
+  </div>
