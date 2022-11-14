e491190b0eccbb65727a388d028a455cd3cd2b70
superstar <83311833+clever-web@users.noreply.github.com>
2022-11-14 09:43:38 +0200

    Update service-messages.blade.php

--- resources/views/components/util/service-messages.blade.php
+++ resources/views/components/util/service-messages.blade.php
@@ -1,13 +1,13 @@
-<section class="container-all-booking mb-3">
-    <div>
-        <div class="rounded-lg p-5">
-            <div class="d-flex flex-column align-items-center">
-                <div class="mb-5"><img class="icon-150px" src="/image/message.png" /></div>
-                <div>
-                    <h4 class="gilroy-bold text-fe2f70 font-22 text-center mb-3">You've got no message</h4>
-                    <p class="gilroy-regular font-12 text-center mb-0">No messages in your inbox, just like every other day. Get some life, dude.</p>												
+    <section class="container-all-booking mb-3">
+        <div>
+            <div class="rounded-lg p-5">
+                <div class="d-flex flex-column align-items-center">
+                    <div class="mb-5"><img class="icon-150px" src="/image/message.png" /></div>
+                    <div>
+                        <h4 class="gilroy-bold text-fe2f70 font-22 text-center mb-3">You've got no message</h4>
+                        <p class="gilroy-regular font-12 text-center mb-0">No messages in your inbox, just like every other day. Get some life, dude.</p>												
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
