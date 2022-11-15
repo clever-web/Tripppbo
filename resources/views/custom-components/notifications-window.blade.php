257f8f6399b7d65b82958e3233926cf321bcdcb6
superstar <83311833+clever-web@users.noreply.github.com>
2022-11-15 10:17:15 +0200

    Update notifications-window.blade.php

--- resources/views/custom-components/notifications-window.blade.php
+++ resources/views/custom-components/notifications-window.blade.php
@@ -1,3 +1,3 @@
-<div id="notifications_app" class="toast-container position-fixed bottom-0 end-0 p-3" style="z-index: 1000; min-width:400px;">
-    <notification-item  v-for="notification in notifications" :title="notification.title" :text="notification.text" :link="notification.link"></notification-item>
-</div>
+    <div id="notifications_app" class="toast-container position-fixed bottom-0 end-0 p-3" style="z-index: 1000; min-width:400px;">
+        <notification-item  v-for="notification in notifications" :title="notification.title" :text="notification.text" :link="notification.link"></notification-item>
+    </div>
